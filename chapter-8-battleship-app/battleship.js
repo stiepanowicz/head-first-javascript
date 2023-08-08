b95227b41d9bcf524232652e7bb852b92f4c97af
air-carbon <stiepanbogdan@gmail.com>
2023-08-08 03:37:15 +0200

    controller

--- chapter-8-battleship-app/battleship.js
+++ chapter-8-battleship-app/battleship.js
@@ -32,7 +32,7 @@ const model = {
                 view.displayHit(guess);
                 view.displayMessage("HIT!");
                 if (this.isSunk(ship)) {
-                    view.displayMessage("YOur sank my battleship!");
+                    view.displayMessage("You sank my battleship!");
                     this.shipSunk++;
                 }
                 return true;
@@ -53,16 +53,52 @@ const model = {
     }
 }
 
-// model.fire("01");
-// model.fire("05");
-// model.fire("53");
-// model.fire("06");
-// model.fire("16");
-// model.fire("26");
-// model.fire("24");
-// model.fire("25");
-// model.fire("34");
-// model.fire("44");
-// model.fire("12");
-// model.fire("11");
-// model.fire("10");
+const controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+        let location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage(`You sank all my battleships, in ${this.guesses} guesses.`);
+            }
+        }
+    }
+}
+
+function parseGuess(guess) {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess == null || guess.length !== 2) {
+        alert("Enter letter and number on the board. (e.g. C3");
+    } else {
+        let firstChar = guess.charAt(0);
+        let row  = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+            alert("that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("that's off the board");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
+controller.processGuess("A0");
+
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
